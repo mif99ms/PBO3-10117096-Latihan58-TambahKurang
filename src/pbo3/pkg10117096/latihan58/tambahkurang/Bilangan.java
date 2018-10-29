d709dfc215b77617519d2699e786a2e91d30fbe8
mif99ms <msidqi86@gmail.com>
2018-10-29 13:28:37 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan58/tambahkurang/Bilangan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan58.tambahkurang;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO 3
+ */
+public class Bilangan {
+     private int x;
+    private int y;
+
+    public Bilangan() {
+        this.x = 3;
+        this.y = 4;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/pbo3/pkg10117096/latihan58/tambahkurang/PBO310117096Latihan58TambahKurang.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan58.tambahkurang;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO 3
+ * Deskripsi    : Program ini berisi program untuk menampilkan hasil penjumlahan
+ * dan hasil selisih
+ */
+public class PBO310117096Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         JumlahBilangan jumlahBil = new JumlahBilangan();
+        SelisihBilangan selisihBil = new SelisihBilangan();
+
+        jumlahBil.tampilHasilJumlah();
+        selisihBil.tampilHasilSelisih();
+    }
+    
+}
